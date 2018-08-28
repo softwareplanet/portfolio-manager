56a4d8a01f0cb076d16572cddcc42f0624473168
Maksym Berezovskyi <maksymbe@interlink-ua.com>
2018-08-28 16:31:26 +0300

    Refactor action types

--- react-client/src/actions/actionTypes.js
+++ react-client/src/actions/actionTypes.js
@@ -1,17 +1,33 @@
+//Boolean fields
 export const TOGGLE_SIDE_BAR = 'TOGGLE_SIDE_BAR';
-export const LOGIN_ERRORS = 'LOGIN_ERRORS';
-export const LOGIN_LOADING = 'LOGIN_LOADING';
-export const REGISTER_ERRORS = 'REGISTER_ERRORS';
-export const REGISTER_LOADING = 'REGISTER_LOADING';
 export const IS_AUTHENTICATED = 'IS_AUTHENTICATED';
-export const SET_USER = 'SET_USER';
-export const SET_USER_PROJECTS = 'SET_USER_PROJECTS';
-export const SET_PROJECTS = 'SET_PROJECTS';
-export const SET_SKILLS = 'SET_SKILLS';
+
+//Set boolean fields for modal windows
 export const SET_PROJECT_MODAL = 'SET_PROJECT_MODAL';
 export const SET_SKILL_MODAL = 'SET_SKILL_MODAL';
-export const ADD_SKILL = 'ADD_SKILL';
+
+//Set operations
+export const SET_USER = 'SET_USER';
+export const SET_SKILLS = 'SET_SKILLS';
+export const SET_PROJECTS = 'SET_PROJECTS';
+export const SET_USER_PROJECTS = 'SET_USER_PROJECTS';
+
+//Loadings
+export const LOGIN_LOADING = 'LOGIN_LOADING';
+export const REGISTER_LOADING = 'REGISTER_LOADING';
 export const NEW_SKILL_LOADING = 'NEW_SKILL_LOADING';
-export const ADD_USER_PROJECT = 'ADD_USER_PROJECT';
-export const DELETE_USER_PROJECT = 'DELETE_USER_PROJECT';
+export const NEW_PROJECT_LOADING = 'NEW_PROJECT_LOADING';
+
+//Errors
+export const LOGIN_ERRORS = 'LOGIN_ERRORS';
+export const REGISTER_ERRORS = 'REGISTER_ERRORS';
 export const CREATE_SKILL_ERRORS = 'CREATE_SKILL_ERRORS';
+export const CREATE_PROJECT_ERRORS = 'CREATE_PROJECT_ERRORS';
+
+//Add to existing arrays actions
+export const ADD_SKILL = 'ADD_SKILL';
+export const ADD_PROJECT = 'ADD_PROJECT';
+export const ADD_USER_PROJECT = 'ADD_USER_PROJECT';
+
+//Delete actions
+export const DELETE_USER_PROJECT = 'DELETE_USER_PROJECT';
