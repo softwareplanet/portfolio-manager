429075cf9696a7c2a9552c2b4dbccfbf9abf480b
Maksym Berezovskyi <maksymbe@interlink-ua.com>
2018-09-13 16:29:07 +0300

    add groupBy method

--- react-client/src/service/utils.js
+++ react-client/src/service/utils.js
@@ -9,3 +9,16 @@ export const formatDate = (d) => {
 
   return [year, month, day].join('-');
 };
+export const groupBy = (list, keyGetter) => {
+  const map = new Map();
+  list.forEach((item) => {
+    const key = keyGetter(item);
+    const collection = map.get(key);
+    if (!collection) {
+      map.set(key, [item]);
+    } else {
+      collection.push(item);
+    }
+  });
+  return Array.from(map);
+};
