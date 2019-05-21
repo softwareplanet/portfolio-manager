7c9ab96bd5a22c31849db7a2f94bad179395f0dc
Maksym Berezovskyi <maksymbe@interlink-ua.com>
2019-05-21 18:09:06 +0300

    Implement paginator class

--- react-client/src/service/utils.js
+++ react-client/src/service/utils.js
@@ -22,3 +22,20 @@ export const groupBy = (list, keyGetter) => {
   });
   return Array.from(map);
 };
+
+export class Paginator {
+  constructor(array, pageSize = 10) {
+    this.array = array;
+    this.pageSize = pageSize;
+  }
+
+  getPagesCount() {
+    if (this.array) {
+      return Math.ceil(this.array.length / this.pageSize);
+    }
+  }
+
+  getCurrentPage(pageNumber) {
+    return this.array.slice(pageNumber * this.pageSize, (pageNumber + 1) * this.pageSize)
+  }
+}
